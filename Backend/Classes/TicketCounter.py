96741bceacbe034df30cc9f1307ab02629c45443
Chase Carthen <chasec2331@gmail.com>
2019-04-25 18:18:14 -0700

    Updating TicketCounter to mark a session in redis.

--- Backend/Classes/TicketCounter.py
+++ Backend/Classes/TicketCounter.py
@@ -6,10 +6,12 @@ redis = Redis(host='redis', port=6379)
 class SessionTicketCounter():
 
     def __init__(self):
-        redis.set('Session Counter',str(uuid.uuid1()))
+        #redis.set('Session Counter',str(uuid.uuid1()))
+        pass
 
 
-    def TakeTicket(self):
-        Ticket = str(redis.get('Session Counter'))
-        redis.set('Session Counter',str(uuid.uuid1()))
+    def TakeTicket(self, fileName=""):
+        #Ticket = str(redis.get('Session Counter'))
+        Ticket = str(uuid.uuid1())
+        redis.set(Ticket, fileName)
         return Ticket
--- Backend/Tests/UnitTests/Services/test_input_mgr.py
+++ Backend/Tests/UnitTests/Services/test_input_mgr.py
@@ -2,6 +2,10 @@ import pytest
 import redis
 import fakeredis
 import io
+import mock
+from mock import mock_open
+import json
+m = mock_open()
 # redis is invoked on start
 redis.Redis = fakeredis.FakeRedis
 
@@ -18,13 +22,17 @@ def test_GettingBasePage(client):
     response.status_code == 200
     response.data == "Hello world"
 
+# mocking open http://omiron.ro/post/python/how_to_mock_open_file/
 # for ref https://stackoverflow.com/questions/35684436/testing-file-uploads-in-flask
 def test_UploadingFileSuccessfully(client):
+
     data = dict(
         file=(io.BytesIO(b'my file contents'), "myfile.csv"),
     )
-    response = client.post('/upload/', data=data, content_type='multipart/form-data')
-    assert response.data == "Success" 
+    with mock.patch('Backend.Services.Input_Mgr.input_mgr.open'):
+        response = client.post('/upload/', data=data, content_type='multipart/form-data')
+        print (json.loads(response.data)[u'status'])
+        assert json.loads(response.data)[u'status'] == "Success" 
 
 def test_UploadingNoFile(client):
     data = dict(
