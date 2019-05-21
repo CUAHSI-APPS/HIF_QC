d40f855051f6f0273762bbbd62d2f98dd25fa9b2
Connor <cscully-allison@nevada.unr.edu>
2019-05-21 16:14:49 -0700

    Further development added a data manager service. Works successully with redis and retrivees files uploaded by the input manager class

--- Backend/Classes/Data.py
+++ Backend/Classes/Data.py
@@ -2,8 +2,9 @@
 ##Contains: Data Manager Class defintion
 ## Authors: Connor Scully-Allison, Chase Carthen
 
-import pandas as pd
 from flask import jsonify
+import json
+import pandas as pd
 
 '''
 Class: Data Manager
@@ -20,16 +21,45 @@ class DataManager():
         return self.redis.get(sessionId)
 
 
-    def readAndLoadData(self, filePath):
+    def readAndLoadData(self, filePath, numRows=None):
         #variables
         dataDict = {}
-        dataJson = ""
 
         with open(filePath, 'r') as file:
             df = pd.read_csv(file)
             for col in list(df.columns):
-                dataDict[col] = list(df[col])
+                if numRows is not None:
+                    dataDict[col] = list(df[col][:numRows])
+                else:
+                    dataDict[col] = list(df[col])
 
-        dataJson = jsonify(dataDict)
+        return dataDict
 
-        return dataJson
+    def retrieveOnlyDataCols(self, filePath, dataColList):
+        # variables
+        dataDict = {}
+
+        with open(filePath, 'r') as file:
+            df = pd.read_csv(file)
+            for col in list(df.columns):
+                if col in dataColList:
+                    dataDict[col] = list(df[col])
+
+        return dataDict
+
+    def storeMetadata(self, sessionId, metaData):
+        metaDataLoc = "MD" + sessionId
+
+        #metadata should be a dictonary
+        self.redis.set(metaDataLoc, jsonify(metaData))
+
+        return
+
+    def retrieveMetadata(self, sessionId):
+        metaDataJson = ""
+        metaDataLoc = "MD" + sessionId
+
+        metaDataJson = self.redis.get(metaDataLoc)
+
+
+        return json.loads(metaDataJson)
--- Backend/Services/DataManager/DataManager.py
+++ Backend/Services/DataManager/DataManager.py
@@ -1,15 +1,15 @@
 import sys
 #sys.path.append("..")
 from flask import Flask, jsonify, json, request
-from flask_cors import cross_origin
-import pyodbc
+from Backend.Classes.Data import DataManager
+from redis import Redis
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+dataManager = DataManager(redis)
 
 # Service Functionality
-@app.route('/Data/')
-@cross_origin()
+@app.route('/')
 def index():
 
 	# Try
@@ -18,12 +18,25 @@ def index():
 		return("working")
 
 	# Except
-	except Exception, e:
+	except e:
 		print(str(e))
 		return False, str(e)
+
+
+@app.route('/cols/<sessionId>', methods=['GET'])
+def get_columns(sessionId):
+    cols = []
+
+    fileName = dataManager.retrieveFileLoc(sessionId)
+    data = dataManager.readAndLoadData(fileName, 20)
+
+    for col in data:
+        cols.append(col)
+
+    return jsonify(cols)
 
 # Run Main
 if __name__ == '__main__':
 	# Set to False when deploying
 	app.debug = True
-	app.run(host='127.0.0.1', port=8082)
+	app.run(host='0.0.0.0', port=8082)
