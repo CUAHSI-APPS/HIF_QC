8af7622d982fcd969046868078d16781bad0f97c
Connor <cscully-allison@nevada.unr.edu>
2019-06-11 18:16:15 -0700

    Development on multiple modules on the configure page. React is more fully integrated in the overall project and a webpack config file has been added.

---
+++ Frontend/HQ_Frontend/src/ConfigMetadataView.jsx
@@ -0,0 +1,108 @@
+'use strict';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+class ConfigMetadataView extends React.Component {
+  static defaultProps = {
+        metaData: {
+            'key': []
+        }
+    };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show:false,
+    };
+
+    this.temp = JSON.parse(JSON.stringify(this.props.metaData));
+
+    this.handleClose = this.handleClose.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.manageInput = this.manageInput.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  manageInput(e){
+    var field = e.target.getAttribute("field");
+
+    this.temp[field] = e.target.value;
+
+    console.log(this.temp, e.target.value, e.target.getAttribute("field"));
+
+  }
+
+  handleClose() {
+      console.log(this.props.metaData);
+      this.temp = null;
+      this.setState({ show: false });
+   }
+
+   handleSave(){
+     for(var field in this.temp){
+       this.props.metaData[field] = this.temp[field];
+     }
+     this.setState({ show: false });
+   }
+
+   handleShow() {
+     //does a deep copy to ensure changes are not propogated back to our main state
+     // if not explicitly saved
+     this.temp = JSON.parse(JSON.stringify(this.props.metaData));
+
+
+
+     this.setState({ show: true });
+   }
+
+  render() {
+
+    return (
+      <>
+        <div className="card-body">
+            <h6>Data Field Metadata</h6>
+            <div className="md-display">
+            <ul>
+              <li>Name: <span>{this.props.selectedCol}</span></li>
+              {Object.keys(this.props.metaData).map( (key, index) => (
+                <li key={'li'+key}>{key} : <span key={key}> {this.props.metaData[key]} </span> </li>
+              ))}
+            </ul>
+            </div>
+            <div className="text-center pt-3">
+                <button id="btnMeta" onClick={this.handleShow} className="btn btn-secondary">Modify Metadata</button>
+            </div>
+        </div>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modify Metadata</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+            <ul>
+              <li>Name: <span>{this.props.selectedCol}</span></li>
+              {Object.keys(this.props.metaData).map( (key, index) => (
+                <li key={'mli'+key}>{key} : <input type='text' key={key} field={key} defaultValue={this.props.metaData[key]} onChange={this.manageInput} /> </li>
+              ))}
+            </ul>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                Close Without Saving
+              </Button>
+              <Button variant="primary" onClick={this.handleSave}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default ConfigMetadataView;
+//
+// let domContainer = document.querySelector('#config-column-list');
+// ReactDOM.render(<ConfigColumnList />, domContainer);
